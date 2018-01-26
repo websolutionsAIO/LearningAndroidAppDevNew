f6abbed232998a61f7a5afe9c5e93c1d69a1368c
WebAppDev <Lenovo@LENOVO-PC>
2018-01-26 23:26:51 +0530

    Add project files.

---
+++ XamarinFormsStarterKit/XamarinFormsStarterKit/MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsStarterKit
+{
+    public partial class MainPage : ContentPage
+    {
+        private ItemViewModel itemsService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.itemsService = new ItemViewModel();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.BusyIndicator.IsVisible = true;
+            this.BusyIndicator.IsRunning = true;
+            try
+            {
+                await this.itemsService.PopulateDataAsync(false);
+                // Data-binding:
+                this.BindingContext = this.itemsService.Items;
+            }
+            catch(InvalidOperationException ex)
+            {
+                await DisplayAlert("Error", "Check your network connection.", "OK");
+                return;
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+                return;
+            }
+            finally
+            {
+                this.BusyIndicator.IsVisible = false;
+                this.BusyIndicator.IsRunning = false;
+            }
+        }
+
+        private async Task LoadDataAsync()
+        {
+            this.BusyIndicator.IsVisible = true;
+            this.BusyIndicator.IsRunning = true;
+            try
+            {
+                await this.itemsService.PopulateDataAsync(true);
+                this.BindingContext = this.itemsService.Items;
+            }
+            catch (InvalidOperationException ex)
+            {
+                await DisplayAlert("Error", "Check your network connection.", "OK");
+                return;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+                return;
+            }
+            finally
+            {
+                this.BusyIndicator.IsVisible = false;
+                this.BusyIndicator.IsRunning = false;
+            }
+        }
+
+        private async void RssView_Refreshing(object sender, EventArgs e)
+        {
+            await LoadDataAsync();
+        }
+
+        private async void RefreshButton_Clicked(object sender, EventArgs e)
+        {
+            await LoadDataAsync();
+        }
+
+        private async void RssView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var selected = e.Item as Item;
+
+            if (selected != null)
+                await Navigation.PushAsync(new WebContentPage(selected.Link));
+        }
+
+        private async void OnAdd(object sender, System.EventArgs e)
+        {
+            var item = new Item { Title = Title.Text, Author = Author.Text ,Fees=Fees.Text};
+            //_connection.InsertAsync(test);
+            //_test.Add(test);
+
+            this.itemsService.SaveItem(item);
+
+            await LoadDataAsync();
+        }
+    }
+}
---
+++ XamarinFormsStarterKit/XamarinFormsStarterKit/WebContentPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsStarterKit
+{
+    public partial class WebContentPage : ContentPage
+    {
+        string link;
+        public WebContentPage(string link)
+        {
+            InitializeComponent();
+
+            this.link = link;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (!App.IsConnected)
+            {
+                // Show a modal message
+                await DisplayAlert("Error", "Check your network connection", "OK");
+
+                // Navigate back
+                await Navigation.PopAsync();
+            }
+
+            // Assign WebView.Source with the html content to show
+            this.WebView1.Source = new Uri(link, UriKind.Absolute);
+        }
+    }
+}
